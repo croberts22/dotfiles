3346cec0eff786b12b4a478042eae90eada3122b
Corey Roberts <spacepyro@gmail.com>
2020-04-29 22:50:52 -0500

    Adding a function to generate tests for a graduate school project.

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -8,3 +8,13 @@ copr() {
 upload_music_to_tenebrae() {
   scp -r $1 spacepyro@tenebrae:music/
 }
+
+test_and_upload() {
+  rm -rf build/Logs/Test
+  xcodebuild -workspace $1 -scheme $2 -derivedDataPath build -destination 'platform=iOS Simulator,OS=13.4,name=iPhone 11' -enableCodeCoverage YES clean build test | xcpretty
+  find build/Logs/Test -name "*.xcresult" | xargs -I % sh -c 'xcrun xccov view --report --json % > %.json'
+  find build/Logs/Test -name "*xcresult*" -exec scp -r {} test_results ';'
+  find test_results -maxdepth 1 -name "*.json" | sort -n | xargs -I % sh -c 'curl -v -H "Content-Type:multipart/form-data" -F "file=@%;type=application/json" localhost:3000/coverage_reports/'
+  find test_results -maxdepth 1 -name "*.json" -exec mv {} test_results/archive ';'
+  find test_results -maxdepth 1 -name "*.xcresult" -exec mv {} test_results/xcresult ';'
+}
